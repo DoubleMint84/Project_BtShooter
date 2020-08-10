8c32c39d3d5f403dd79041440ab72a6ebfb91558
doublemint84 <nikitka.borodavko@gmail.com>
2020-08-10 14:20:18 +0300

    Fixed call-response algorythm

--- Code/Base_node_code/Base_node_code.ino
+++ Code/Base_node_code/Base_node_code.ino
@@ -11,7 +11,7 @@ unsigned long nodeTimeout = 0;
 GButton but(BTN_PIN, HIGH_PULL);
 GyverOLED oled;
 SoftwareSerial btSerial(3, 2); // RX, TX
-RF24 radio(7, 8);               // nRF24L01 (CE,CSN)
+RF24 radio(7, 9);               // nRF24L01 (CE,CSN)
 RF24Network network(radio);      // Include the radio in the network
 const uint16_t this_base = 00;   // Address of this node in Octal format ( 04,031, etc)
 const uint16_t node01 = 01;
@@ -22,14 +22,17 @@ bool receivedFlag = false;
 
 void setup() {
   SPI.begin();
+  Serial.begin(9600);
   btSerial.begin(9600);
   radio.begin();
+  pinMode(5, OUTPUT);
+  digitalWrite(5, LOW);
   network.begin(90, this_base);  //(channel, node address)
   oled.init(OLED128x64, 800);
   oled.setContrast(1);
   oled.clear();      // очистить
   oled.home();      // курсор в 0,0
-    oled.scale1X();    // масштаб шрифта х1  
+  oled.scale1X();    // масштаб шрифта х1
   oled.print("Привет, ");
   oled.update();
   delay(2000);
@@ -43,42 +46,52 @@ void loop() {
     RF24NetworkHeader header;
     network.read(header, &incomingData, sizeof(incomingData)); // Read the incoming data
     receivedFlag = true;
+    Serial.print(incomingData);
+    Serial.print(' ');
+    Serial.println(nodeState);
   }
-  switch (nodeState) {
-    case 0:
-      nodeState = 1;
-      data = 1;
-      RF24NetworkHeader header(node01);     // (Address where the data is going)
-      bool ok = network.write(header, &data, sizeof(data)); // Send the data
+  if (nodeState == 0) {
+    nodeState = 1;
+    data = 1;
+    RF24NetworkHeader header(node01);     // (Address where the data is going)
+    bool ok = network.write(header, &data, sizeof(data)); // Send the data
+    dispUpdate = true;
+    nodeTimeout = millis();
+    Serial.println("CASE 0");
+  } else if (nodeState == 1) {
+    if (receivedFlag) {
+      Serial.println("received");
+      nodeState = 2;
       dispUpdate = true;
-      break;
-    case 1:
-      if (receivedFlag) {
-        if (incomingData == 1) {
-          nodeState = 2;
-          dispUpdate = true;
-          nodeTimeout = millis();
-        } 
-        receivedFlag = false;
-              
-      }
-      break;
-    case 2:
-      if (receivedFlag) {
-        nodeTimeout = millis();
-        receivedFlag = false;
-      } else if (millis() - nodeTimeout >= period_time) {
-        nodeState = 0;
-        dispUpdate = true;
-      }
-      break;
+      nodeTimeout = millis();
+      digitalWrite(5, HIGH);
+      receivedFlag = false;
+
+    } else if (millis() - nodeTimeout >= period_time) {
+      nodeState = 0;
+      digitalWrite(5, LOW);
+      dispUpdate = true;
+    }
+    Serial.println("CASE 1");
+  } else if (nodeState == 2) {
+    if (receivedFlag) {
+      nodeTimeout = millis();
+      receivedFlag = false;
+    } else if (millis() - nodeTimeout >= period_time) {
+      nodeState = 0;
+      digitalWrite(5, LOW);
+      dispUpdate = true;
+    }
+   // Serial.println("CASE 2");
   }
+
   if (dispUpdate) {
     oled.clear();      // очистить
     oled.home();      // курсор в 0,0
-    oled.scale1X();    // масштаб шрифта х1  
+    oled.scale1X();    // масштаб шрифта х1
     oled.print(nodeState);
     oled.update();
+    dispUpdate = false;
   }
-  
+
 }
--- Code/Remote_node_code/Remote_node_code.ino
+++ Code/Remote_node_code/Remote_node_code.ino
@@ -4,20 +4,22 @@
 #include <GyverButton.h>
 
 #define BTN_PIN 2
-const unsigned long period_time = 3000;
+const unsigned long period_time = 1500;
 unsigned long nodeTimeout = 0;
 GButton but(BTN_PIN, HIGH_PULL);
-RF24 radio(7, 8);               // nRF24L01 (CE,CSN)
+RF24 radio(9, 10);               // nRF24L01 (CE,CSN)
 RF24Network network(radio);      // Include the radio in the network
 const uint16_t this_node = 01;   // Address of this node in Octal format ( 04,031, etc)
 const uint16_t base00 = 00;
 byte incomingData, data;
+bool receivedFlag = false;
 
 void setup() {
   // put your setup code here, to run once:
   SPI.begin();
   radio.begin();
   network.begin(90, this_node);  //(channel, node address)
+  nodeTimeout = millis();
 }
 
 void loop() {
@@ -25,12 +27,15 @@ void loop() {
   network.update();
   but.tick();
   while ( network.available() ) {     // Is there any incoming data?
-    RF24NetworkHeader header;
-    network.read(header, &incomingData, sizeof(incomingData)); // Read the incoming data
-    if (incomingData == 1) {
-      RF24NetworkHeader header(base00);     // (Address where the data is going)
-      data = 1;
-      bool ok = network.write(header, &data, sizeof(data)); // Send the data
-    }
+    RF24NetworkHeader headerIn;
+    network.read(headerIn, &incomingData, sizeof(incomingData)); // Read the incoming data
+    receivedFlag = true;
+  }
+  if (receivedFlag || millis() - nodeTimeout >= period_time) {
+    RF24NetworkHeader header(base00);     // (Address where the data is going)
+    data = 1;
+    bool ok = network.write(header, &data, sizeof(data)); // Send the data
+    nodeTimeout = millis();
+    receivedFlag = false;
   }
 }
